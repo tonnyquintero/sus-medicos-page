73f279f7255a5e4a965ad790c322bc496183df7f
tonny187 <tonyoanthony@gmail.com>
2024-01-30 12:46:11 -0500

    adding more meta data

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,20 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta charset="UTF-8" />
+        <meta name='robots' content='index, follow, max-image-preview:large, max-snipets:-1' />
         <title>Sus Médicos IPS, EXAMENES MEDICOS OCUPACIONALES EN MEDELLIN</title>
         <meta name="description" content="IPS EXAMENES MEDICOS OCUPACIONALES EN MEDELLIN, IPS EXÁMENES OCUPACIONALES MEDELLIN, EXAMENES MÉDICOS MEDELLIN, EXÁMENES DE LABORATORIO EN MEDELLIN, Conciencia Laboral y Valoración" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:locale' content='es_ES' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Sus Médicos IPS, EXAMENES MEDICOS OCUPACIONALES EN MEDELLIN' />
+        <meta property='og:description' content='IPS EXAMENES MEDICOS OCUPACIONALES EN MEDELLIN, IPS EXÁMENES OCUPACIONALES MEDELLIN, EXAMENES MÉDICOS MEDELLIN, EXÁMENES DE LABORATORIO EN MEDELLIN, Conciencia Laboral y Valoración' />
+        <meta property='og:url' content='https://sus-medicos.com' />
+        <meta property='og:site_name' content='susmedicos' />
+        <meta property='og:image' content='https://www.sus-medicos.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsus1.e20ce011.jpg&w=640&q=75' />
         <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel='canonical' href='https://sus-medicos.com' />
       </Head>
       <div className="containerFlag">
                   <button className='btn-bg' onClick={() => i18n.changeLanguage("es")}>
